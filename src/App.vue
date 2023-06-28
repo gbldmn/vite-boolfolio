4f89797edf6e161799ebe2779c3d60a1dc138e5c
glbdmn <gabrieledamiani@hotmail.it>
2023-06-28 13:53:50 +0200

    fatta la chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script>
+import axios from 'axios';
+
+export default{
+    data(){
+        return{
+            projects: [],
+            baseUrl: 'localhost:8000/api'
+        }
+    },
+    mounted(){
+        this.getProjects();
+    },
+    methods: {
+        getProjects(){
+            axios.get( `${this.baseUrl}/projects` )
+                .then( res => {
+                    this.projects = res.data.projects
+                } )
+        }
+    },
+}
 
 </script>
 
 <template>
-
+<h1>ciao</h1>
   
 </template>
 
